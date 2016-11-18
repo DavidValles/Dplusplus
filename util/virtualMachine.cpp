6316326a4c98ad6ed4274d7566762b8f2f3b99f7
David Valles <davs93@hotmail.com>
2016-11-18 11:56:48 -0600

    first version of virtualMachine.cpp

---
+++ util/virtualMachine.cpp
@@ -0,0 +1,13 @@
+#ifndef VIRTUALMACHINE_CPP
+#define VIRTUALMACHINE_CPP
+
+#include "headers/virtualMachine.h"
+using namespace std;
+
+VirtualMachine::VirtualMachine(){
+
+}
+
+void VirtualMachine::run(){
+	
+}
